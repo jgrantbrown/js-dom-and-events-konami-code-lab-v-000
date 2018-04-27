4e2ddadb9bf2291457c37e6988a93903f7390f66
JGBrown <j.grant.brown@gmail.com>
2018-04-27 10:49:35 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,32 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+               // Uparrow(38)
+               // Uparrow(38)
+               // DownArrow (40)
+               // DownArrow (40)
+               // LeftArrow (37)
+               // RightArrow (39)
+               // LeftArrow (37)
+               // RightArrow (39)
+               // B key (66)
+               // A Key (65)
 
 function init() {
+  let index = 0;
   // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(e){
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+        
+    }
+  })
 
 }
